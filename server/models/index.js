90e80a58f4aa9df748facfe6d65830c522f51e66
JohnCharamonde <john.charamonde@gmail.com>
2020-01-09 23:04:42 -0500

    Progressing through databases.

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,50 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      var queryStr = 'select messages.id, messages.text, messages.roomname, users.username from messages left outer join users on (messages.userid = users.id) order by messages.id desc';
+      db.query(queryStr, function (err, results) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(err, results);
+        }
+      });
+    }, // a function which produces all the messages
+    post: function (params, callback) {
+      var queryStr = 'insert into messages(text, userid, roomname) values (?, (select id from users where username = ? limit 1), ?)';
+      db.query(queryStr, params, function (err, results) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, results);
+        }
+      });
+    } // a function which can be used to insert a message into the database
   },
-
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      var queryString = 'select * from users';
+      db.query(queryString, (err, result) => {
+        if (err) {
+          console.log(err, null);
+        } else {
+          callback(null, result);
+        }
+      });
+    },
+
+    post: function (params, callback) {
+      var queryString = 'insert into users(username) values (?)';
+      db.query(queryString, (err, result) => {
+        if (err) {
+          console.log(err, null);
+        } else {
+          callback(null, result);
+        }
+      });
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -3,21 +3,21 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE users (
-  user_id int not null,
-  user_name varchar(255),
+  user_id int NOT NULL AUTO_INCREMENT,
+  user_name varchar(255) NOT NULL,
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE rooms (
-  room_id int not null,
+  room_id int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (room_id)
 );
 
 CREATE TABLE messages (
-  message_id int not null,
-  user_id int not null,
-  room_id int not null,
-  message_content varchar(255),
+  message_id int NOT NULL AUTO_INCREMENT,
+  user_id int NOT  NULL,
+  room_id int NOT NULL,
+  message_content varchar(255) NOT NULL,
   PRIMARY KEY (message_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (room_id) REFERENCES rooms(room_id)
